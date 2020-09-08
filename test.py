bcb75591b14319e9b7c3f15d92ff6659c6e5b114
zzsyjl <287683119@qq.com>
2020-09-08 20:20:13 +0800

    init

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 # this is just try
 # this is the second try
+# while i m coding, things are changing. do I have autosave? you know thisline
+# hasn't been saved
+# this one shouldn't be saved
+# will you have something different?
+
+# 这次总不能存上了把？
+# 这下你总不需要着急了
+
+
+
